a9774ee88ca548ae1e00c56ea92360746dd01fb1
Tsimafei Shostka <tsimafei.shostka@LeverX.local>
2019-10-16 11:42:48 +0300

    add source files and pom-file

---
+++ src/main/java/com/leverx/config/WebAppInitializer.java
@@ -0,0 +1,20 @@
+package com.leverx.config;
+
+import org.springframework.web.WebApplicationInitializer;
+import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
+import org.springframework.web.servlet.DispatcherServlet;
+
+import javax.servlet.ServletContext;
+import javax.servlet.ServletRegistration;
+
+public class WebAppInitializer implements WebApplicationInitializer {
+
+    public void onStartup(ServletContext servletContext) {
+        AnnotationConfigWebApplicationContext context = new AnnotationConfigWebApplicationContext();
+        context.register(WebConfig.class);
+        context.setServletContext(servletContext);
+        ServletRegistration.Dynamic dispather = servletContext.addServlet("dispacher", new DispatcherServlet(context));
+        dispather.setLoadOnStartup(1);
+        dispather.addMapping("/");
+    }
+}
